8842fbaab72a4b72c875d8891b4facbadab747cc
Jack <jk19011999@gmail.com>
2023-07-28 22:22:03 +0530

    Created basic structure for the project

---
+++ src/main/java/com/thejackfolio/microservices/thejackfolio_db/ThejackfolioDbApplication.java
@@ -0,0 +1,13 @@
+package com.thejackfolio.microservices.thejackfolio_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThejackfolioDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThejackfolioDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thejackfolio/microservices/thejackfolio_db/ThejackfolioDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejackfolio.microservices.thejackfolio_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThejackfolioDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
