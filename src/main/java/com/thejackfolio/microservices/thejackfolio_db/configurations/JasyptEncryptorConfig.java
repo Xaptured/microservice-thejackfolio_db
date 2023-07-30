b7455b543ccee8e26c2a6bd0a6827ee629a81cf9
Jack <jk19011999@gmail.com>
2023-07-31 02:33:54 +0530

    Added encryption utilities

---
+++ src/main/java/com/thejackfolio/microservices/thejackfolio_db/configurations/JasyptEncryptorConfig.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023.
+ * Created this for the project called "TheJackFolio"
+ * All right reserved by Jack
+ */
+
+package com.thejackfolio.microservices.thejackfolio_db.configurations;
+
+import com.thejackfolio.microservices.thejackfolio_db.utilities.PropertiesReader;
+import com.thejackfolio.microservices.thejackfolio_db.utilities.StringConstants;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The class type Jasypt encryptor config.
+ */
+@Configuration
+public class JasyptEncryptorConfig {
+
+    /**
+     * Gets password encryptor.
+     *
+     * @return the password encryptor
+     */
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor getPasswordEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(PropertiesReader.getProperty(StringConstants.ENCRYPTION_PASSWORD));
+        config.setAlgorithm(PropertiesReader.getProperty(StringConstants.ENCRYPTION_ALGORITHM));
+        config.setKeyObtentionIterations(PropertiesReader.getProperty(StringConstants.ENCRYPTION_ITERATIONS));
+        config.setPoolSize(PropertiesReader.getProperty(StringConstants.ENCRYPTION_POOL_SIZE));
+        config.setProviderName(PropertiesReader.getProperty(StringConstants.ENCRYPTION_PROVIDER_NAME));
+        config.setSaltGeneratorClassName(PropertiesReader.getProperty(StringConstants.ENCRYPTION_SALT_GENERATOR));
+        config.setStringOutputType(PropertiesReader.getProperty(StringConstants.ENCRYPTION_OUTPUT_TYPE));
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
---
+++ src/main/java/com/thejackfolio/microservices/thejackfolio_db/utilities/StringConstants.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023.
+ * Created this for the project called "TheJackFolio"
+ * All right reserved by Jack
+ */
+
+package com.thejackfolio.microservices.thejackfolio_db.utilities;
+
+/**
+ * The class type String constants.
+ */
+public class StringConstants {
+
+    /**
+     * The constant ENCRYPTION_PASSWORD.
+     */
+    public static final String ENCRYPTION_PASSWORD = "ENCRYPTION_PASSWORD";
+    /**
+     * The constant ENCRYPTION_ALGORITHM.
+     */
+    public static final String ENCRYPTION_ALGORITHM = "ENCRYPTION_ALGORITHM";
+    /**
+     * The constant ENCRYPTION_ITERATIONS.
+     */
+    public static final String ENCRYPTION_ITERATIONS = "ENCRYPTION_ITERATIONS";
+    /**
+     * The constant ENCRYPTION_POOL_SIZE.
+     */
+    public static final String ENCRYPTION_POOL_SIZE = "ENCRYPTION_POOL_SIZE";
+    /**
+     * The constant ENCRYPTION_PROVIDER_NAME.
+     */
+    public static final String ENCRYPTION_PROVIDER_NAME = "ENCRYPTION_PROVIDER_NAME";
+    /**
+     * The constant ENCRYPTION_SALT_GENERATOR.
+     */
+    public static final String ENCRYPTION_SALT_GENERATOR = "ENCRYPTION_SALT_GENERATOR";
+    /**
+     * The constant ENCRYPTION_OUTPUT_TYPE.
+     */
+    public static final String ENCRYPTION_OUTPUT_TYPE = "ENCRYPTION_OUTPUT_TYPE";
+
+}
