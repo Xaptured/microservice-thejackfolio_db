2c91a2cb78163fd6dbf89915094ab8c2bee9fffc
Jack <jk19011999@gmail.com>
2023-09-04 19:08:45 +0530

    Fixed Bug

--- src/main/java/com/thejackfolio/microservices/thejackfolio_db/configurations/WebSecurityConfig.java
+++ src/main/java/com/thejackfolio/microservices/thejackfolio_db/configurations/WebSecurityConfig.java
@@ -13,12 +13,12 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
@@ -31,15 +31,15 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, MvcRequestMatcher.Builder mvc) throws Exception {
+        httpSecurity.headers(headers -> headers.frameOptions(options -> options.disable()));
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(mvc.pattern("/actuator/**")).hasRole(ROLE)
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
                         .anyRequest().permitAll()
                 )
                 .formLogin(Customizer.withDefaults())
-                .logout(logout -> logout.logoutUrl("/logout").invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID"))
                 .build();
     }
 
