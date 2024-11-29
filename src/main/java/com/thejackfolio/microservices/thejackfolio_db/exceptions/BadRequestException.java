c38a75751828e032f2bbb3d8f9aaf93267c2850c
Jack <jk19011999@gmail.com>
2024-11-29 19:24:55 +0530

    Updated flow for save new event

---
+++ src/main/java/com/thejackfolio/microservices/thejackfolio_db/exceptions/BadRequestException.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024.
+ * Created this for the project called "TheJackFolio"
+ * All right reserved by Jack
+ */
+
+package com.thejackfolio.microservices.thejackfolio_db.exceptions;
+
+public class BadRequestException extends RuntimeException{
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
